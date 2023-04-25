30c9c02d2d98547b9d71ce93e87f430241673c1e
Syed Abdul Qadir Gilani <66156655+SyedAbdulQadirGilani001@users.noreply.github.com>
2023-04-26 03:22:54 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const toggleButton = document.getElementById('toggle-button');
-
-toggleButton.addEventListener('click', () => {
-  document.body.classList.toggle('dark');
+const speakButton = document.getElementById('speak-button'); // Get the speak button
+const textToSpeak = document.getElementById('text-to-speak'); // Get the text to speak input
+const message = new SpeechSynthesisUtterance(); // Create a new SpeechSynthesisUtterance object
+message.voice = speechSynthesis.getVoices().find(voice => voice.lang === 'en-US'); // Set the voice to use
+message.pitch = 1; // Set the pitch
+message.rate = .5; // Set the rate
+speakButton.addEventListener('click', () => { // Add a click event listener to the speak button
+  message.text = textToSpeak.value; // Set the text to speak
+  speechSynthesis.speak(message); // Speak the text
 });
